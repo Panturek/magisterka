2608fa0abfed97c96a23105ece45b4e1ceda00e8
Panturek <j.halasinski@gmail.com>
2021-09-24 18:27:21 +0200

    Desaggregation class added with some initial code

---
+++ desaggregation.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+
+class Desaggregation():
+    "contains methods for desaggregation data processing"
+    def __init__(self, criteria, demand, names):
+        self.criteria = criteria
+        self.demand = demand
+        self.criteria_pct = criteria.pct_change()
+        self.demand_pct = demand.pct_change()
+        self.names = names
+        self.parameters = {}
+
+    def set_parameter(self, name, value):
+        self[name] = value
+
+    def _set_lvls(self, _range, count):
+        return np.linspace(
+            np.min(_range, axis=0),
+            np.max(_range, axis=0),
+            count)
+    
+    def _set_cost_criteria(self, cri_idcs):
+        for idx in cri_idcs:
+            self.cri_lvls[idx].sort_series(ascending=False,
+                                            inplace=True)
+
+
+    def set_levels(self, criteria_ranges, demand_ranges):
+        self.cri_lvls = self._set_lvls(criteria_ranges, self.alpha_i )
+        self.dem_lvls = self._set_lvls(demand_ranges, self.alpha )
+        if self.cost_criteria:
+            self._set_cost_criteria()
