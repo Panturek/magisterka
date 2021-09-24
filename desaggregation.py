a7897548c194bd0c3e67a12b41bf651f4ccb4ab7
Panturek <j.halasinski@gmail.com>
2021-09-24 18:52:44 +0200

    made it somehow work

--- desaggregation.py
+++ desaggregation.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-class Desaggregation():
+class Desaggregation:
     "contains methods for desaggregation data processing"
     def __init__(self, criteria, demand, names):
         self.criteria = criteria
@@ -9,10 +9,15 @@ class Desaggregation():
         self.criteria_pct = criteria.pct_change()
         self.demand_pct = demand.pct_change()
         self.names = names
-        self.parameters = {}
+        self._params = {}
 
-    def set_parameter(self, name, value):
-        self[name] = value
+    def __getitem__(self, index):
+        if index in self._params.keys():
+            return self._params[index]
+        return None
+    
+    def __setitem__(self, index, value):
+        self._params[index] = value
 
     def _set_lvls(self, _range, count):
         return np.linspace(
@@ -30,4 +35,4 @@ class Desaggregation():
         self.cri_lvls = self._set_lvls(criteria_ranges, self.alpha_i )
         self.dem_lvls = self._set_lvls(demand_ranges, self.alpha )
         if self.cost_criteria:
-            self._set_cost_criteria()
+            self._set_cost_criteria(self.cost_criteria)
